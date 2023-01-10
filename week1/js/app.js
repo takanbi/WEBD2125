668408210c3a963afe1550d0564183acb4adbee3
takanbi <tunji.akanbi@century.edu>
2023-01-10 12:45:45 -0600

    basic XMLTttpRequest works

---
+++ week1/js/app.js
@@ -0,0 +1,18 @@
+
+function getData() {
+    const myDataRequest = new XMLHttpRequest();
+    // const myDataRequest 
+
+    // if(window.XMLHttpRequest) {
+    //     myDataRequest = new XMLHttpRequest();
+    // }
+    // else{
+    //     myDataRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    // }
+
+    myDataRequest.onload = function() {
+        document.getElementById("text").innerHTML = this.responseText;
+    }
+    myDataRequest.open("GET", "data.txt", true);
+    myDataRequest.send();
+}
